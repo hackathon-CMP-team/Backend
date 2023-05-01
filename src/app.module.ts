88218218b3a98856a046aa5c32057c7197399a08
omarfareed23 <omar.abdelaaty02@eng-st.cu.edu.eg>
2023-05-01 12:27:11 +0200

    user resource created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DB_CONNECTION_STRING),
+    UserModule,
   ],
   controllers: [],
   providers: [],

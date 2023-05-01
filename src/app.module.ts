c74a8a0e934763f16618604d4e5378c968179f84
omarfareed23 <omar.abdelaaty02@eng-st.cu.edu.eg>
2023-05-01 12:31:24 +0200

    create auth resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DB_CONNECTION_STRING),
     UserModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],

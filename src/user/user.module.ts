40555eb961c812b95981c92562776a0d51da47f9
omarfareed23 <omar.abdelaaty02@eng-st.cu.edu.eg>
2023-05-02 08:19:33 +0200

    fix test

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './user.schema';
+import {
+  UserDependingControllers,
+  UserDependingModules,
+  UserDependingServices,
+} from './utils/dependencies';
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-  ],
-  controllers: [UserController],
-  providers: [UserService],
+  imports: UserDependingModules,
+  controllers: UserDependingControllers,
+  providers: UserDependingServices,
 })
 export class UserModule {}
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,25 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { rootMongooseTestModule } from '../utils/mongoose-in-memory';
 import { UserService } from './user.service';
+import {
+  UserDependingControllers,
+  UserDependingModules,
+  UserDependingServices,
+} from './utils/dependencies';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      imports: [
+        ConfigModule.forRoot(),
+        rootMongooseTestModule(),
+        ...UserDependingModules,
+      ],
+      controllers: UserDependingControllers,
+      providers: UserDependingServices,
     }).compile();
 
     service = module.get<UserService>(UserService);

8730dd8a7550160ddb2b2940d53a69213b091da1
omarfareed23 <omar.abdelaaty02@eng-st.cu.edu.eg>
2023-05-07 13:41:41 +0200

    initialize children module

--- src/children/children.module.ts
+++ src/children/children.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ChildrenService } from './children.service';
 import { ChildrenController } from './children.controller';
+import {
+  ChildrernDependingControllers,
+  ChildrernDependingModules,
+  ChildrernDependingServices,
+} from './utils/dependencies';
 
 @Module({
-  controllers: [ChildrenController],
-  providers: [ChildrenService]
+  imports: ChildrernDependingModules,
+  controllers: ChildrernDependingControllers,
+  providers: ChildrernDependingServices,
 })
 export class ChildrenModule {}

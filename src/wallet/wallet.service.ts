f1499cd90329abc474a65f97996f2017992cd88f
omarfareed23 <omar.abdelaaty02@eng-st.cu.edu.eg>
2023-05-09 22:37:38 +0200

    wallet functional documentation

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -8,6 +8,11 @@ export class WalletService {
     private readonly userService: UserService,
     private readonly transactionService: TransactionService,
   ) {}
+  /**
+    Get the balance of a user by adding their current balance and unused balance.
+    @param phoneNumber The phone number of the user whose balance is being fetched.
+    @returns An object containing the total balance of the user.
+    */
   async getBalance(phoneNumber: string) {
     const [userBalance, unusedBalance] = await Promise.all([
       this.userService.getUserBalance(phoneNumber),
@@ -17,11 +22,22 @@ export class WalletService {
       total: userBalance + unusedBalance,
     };
   }
+
+  /**
+    Get the total income of a user.
+    @param phoneNumber The phone number of the user whose income is being fetched.
+    @returns An object containing the total income of the user.
+    */
   async getIncome(phoneNumber: any): Promise<any> {
     const totalIncome = await this.transactionService.getIncome(phoneNumber);
     return { total: totalIncome };
   }
 
+  /**
+    Get the total outcome of a user.
+    @param phoneNumber The phone number of the user whose outcome is being fetched.
+    @returns An object containing the total outcome of the user.
+    */
   async getOutcome(phoneNumber: any): Promise<any> {
     const totalOutcome = await this.transactionService.getOutcome(phoneNumber);
     return { total: totalOutcome };
